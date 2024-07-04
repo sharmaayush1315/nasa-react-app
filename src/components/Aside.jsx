68d8ce9befd015795853d05b9851d61d51590ae2
Ayush Sharma <sharmaayush1315@gmail.com>
2024-07-05 02:41:54 +0530

    Update Aside.jsx

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -7,12 +7,7 @@ export default function Aside(props) {
 				<h2>{data?.title}</h2>
 				<div className="descriptionContanier">
 					<p className="descriptionTitle">{data?.date}</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-						eligendi facilis incidunt, quis vero ipsam aspernatur eius aperiam
-						debitis temporibus! Officia laudantium nostrum expedita cumque
-						consectetur, architecto animi quis non!
-					</p>
+					<p>{data?.explanation}</p>
 				</div>
 				<button onClick={handleToggleModal}>
 					<i className="fa-solid fa-arrow-right"></i>
